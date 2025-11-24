d46dacc12d0323576d0ff62e8320384544fa86f1
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-11-24 20:39:38 +0000

    modification: saved all colours used in code into 3 variables defined at the top of the program

--- GenshinOrganiser.py
+++ GenshinOrganiser.py
@@ -9,13 +9,25 @@ from tkinter import ttk
 import pandas as pd
 import os
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg # Used for the piechart
-from matplotlib.figure import Figure #Also used for the figure creation
+from matplotlib.figure import Figure # Also used for the figure creation
 
+# ---------------------------------------------------------------------
+# Defined colours used in program
+#  -> make colour scheme easier to change
+# ---------------------------------------------------------------------
 
-class Character: #character class to hold character data and create og CSV purrr
-    characters = [] #empty list to hold chars
-    character_count = 0
+colour_1 =  "#222437" 
+colour_2 = "#7FB1CE"
+colour_3= "#2c77d8"
 
+# ---------------------------------------------------------------------
+# CLASS: character data 
+# ---------------------------------------------------------------------
+
+class Character: # Character class to hold character data and create CSV
+    characters = [] # Empty list to hold chars
+    character_count = 0 # Counter for total number of characters
+ 
     def __init__(self, name, element, character_star, character_level, max_hp, base_ATK, elemental_skill, elemental_burst, region, weapon_owned):
         self.name = name
         self.element = element
@@ -27,7 +39,7 @@ class Character: #character class to hold character data and create og CSV purrr
         self.elemental_burst = elemental_burst
         self.region = region
         self.weapon_owned = weapon_owned
-        Character.characters.append(self) #for the character list to make sure its added to the intiital csv file
+        Character.characters.append(self) # New characaters appended to genshinOrganiser.csv
 
     @staticmethod 
     def make_characters_csv(): 
@@ -41,25 +53,30 @@ class Character: #character class to hold character data and create og CSV purrr
             character_table.to_csv('genshinCharacters.csv') #then a pd data frame to  csv   
         return pd.read_csv('genshinCharacters.csv') #returns the resultant so it can be used if necessay    
 
+# ---------------------------------------------------------------------
+# CLASS: Window of genshin data
+# ---------------------------------------------------------------------
+
 # GenshinSorter class with treeview stuff yahhh baby
+
 class GenshinSorter:
     def __init__(self, genshin_window):
         self.genshin_window = genshin_window 
         self.genshin_window.title("Genshin Organiser") 
-        self.genshin_window.geometry("%dx%d+%d+%d" % (1300, 750, 100, 50)) #width height xoffset yoffset
-        self.genshin_window.resizable(True, True) #both width and height can be resized
+        self.genshin_window.geometry("%dx%d+%d+%d" % (1300, 750, 100, 50)) # Width, height, xoffset, yoffset
+        self.genshin_window.resizable(True, True) # Both width and height can be resized
 
-#the hottification of treeview stuff 
+        #Stylising tkinter window
         self.style = ttk.Style()
-        self.style.theme_use("clam")  #lets u customise moreeee lifesaverrr
-        self.style.configure("Treeview", 
-                           background= "#222437"  ,
-                           foreground= "#7FB1CE",
-                           fieldbackground = "#222437",
-                           font=("Arial", 12, "bold") #this is the size of the treeview elements                     
+        self.style.theme_use("clam")  # Allows more customisation
+        self.style.configure("Treeview",  # Neuvillette's colour pallette from Genshin
+                           background= colour_1,
+                           foreground= colour_2,
+                           fieldbackground = colour_1,
+                           font=("Arial", 12, "bold") # This is the size of the treeview elements                    
                              )  
         self.style.map('Treeview',
-                      background=[('selected', '#2852B3')]) #changes the color of selected text to #2852B3 (blue)
+                      background=[('selected', '#2852B3')]) # Changes the color of selected text to #2852B3 (blue)
                                 #even hotter button stuff
         self.style.configure(
             "Custom.TButton", #placing this style in all ttk buttons customises them like this 
@@ -68,9 +85,9 @@ class GenshinSorter:
             background="#253365",  #background button color
     )
         # Pages stuff #all pages set as a frame so i could then set a color because i couldnt change the color of the window since it would keep following my evil mac settings
-        self.page1 = tk.Frame(self.genshin_window, bg = "#7FB1CE") 
-        self.page2 = tk.Frame(self.genshin_window, bg = "#7FB1CE" )
-        self.page3 = tk.Frame(self.genshin_window, bg = "#7FB1CE")
+        self.page1 = tk.Frame(self.genshin_window, bg = colour_2) 
+        self.page2 = tk.Frame(self.genshin_window, bg = colour_2)
+        self.page3 = tk.Frame(self.genshin_window, bg = colour_2)
 
         self.frame_page_1buttons = ttk.Frame(self.page1) #frame for my page 1 buttons
         self.frame_page_1buttons.pack(side=BOTTOM, pady = 15)
@@ -96,77 +113,77 @@ class GenshinSorter:
         self.page3_to_page_1.grid(row = 0, column = 0)
 
         #TITLES FOR PAGE 2 AND 3
-        self.page2_title = tk.Label(self.page2, text="SORT CHARACTERS", bg="#7FB1CE", font=("Ariel",30, "bold"))
+        self.page2_title = tk.Label(self.page2, text="SORT CHARACTERS", bg=colour_2 , font=("Ariel",30, "bold"))
         self.page2_title.pack(side= TOP)
-        self.page2_search= tk.Label(self.page2, text= "You can search for: Name, Element, Star, Region and Weapon ;)",font=("Ariel",14, "bold"), bg="#7FB1CE")
+        self.page2_search= tk.Label(self.page2, text= "You can search for: Name, Element, Star, Region and Weapon ;)",font=("Ariel",14, "bold"), bg=colour_2)
         self.page2_search.pack(side=TOP)
-        self.page2_advice= tk.Label(self.page2, text= "Press 'Search' with the search box empty to get all characters",font=("Ariel",14, "bold"), bg="#7FB1CE")
+        self.page2_advice= tk.Label(self.page2, text= "Press 'Search' with the search box empty to get all characters",font=("Ariel",14, "bold"), bg=colour_2)
         self.page2_advice.pack(side=TOP)
 
-        self.page3_title = tk.Label(self.page3, text="EDIT CHARACTERS", font=("Ariel",30, "bold"), bg="#7FB1CE" )
+        self.page3_title = tk.Label(self.page3, text="EDIT CHARACTERS", font=("Ariel",30, "bold"), bg=colour_2)
         self.page3_title.pack(side= TOP)
-        self.info_label= tk.Label(self.page3, text= "To view edits on page 2, close the window and reopen :D",font=("Ariel",14, "bold"), bg="#7FB1CE")
+        self.info_label= tk.Label(self.page3, text= "To view edits on page 2, close the window and reopen :D",font=("Ariel",14, "bold"), bg=colour_2)
         self.info_label.pack(side=TOP)
 
         #This is a place holder ish error message for when boxes are empty for certain buttons
-        self.error_label_2 = tk.Label(self.page2, text="", font=("Ariel",15, "bold"), bg="#7FB1CE", fg="#222437" )
+        self.error_label_2 = tk.Label(self.page2, text="", font=("Ariel",15, "bold"), bg=colour_2 , fg=colour_1 )
         self.error_label_2.pack(side=BOTTOM)
 
-        self.error_label_3 = tk.Label(self.page3, text="", font=("Ariel",15, "bold"), bg="#7FB1CE", fg="#222437" )
+        self.error_label_3 = tk.Label(self.page3, text="", font=("Ariel",15, "bold"), bg=colour_2 , fg=colour_1 )
         self.error_label_3.pack(side=BOTTOM)
 
         #frame for editting entry boxes
-        self.frame_editting = tk.Frame(self.page3, bg="#7FB1CE")
+        self.frame_editting = tk.Frame(self.page3, bg=colour_2)
         self.frame_editting.pack(side=BOTTOM)
  
-        self.name_label = Label(self.frame_editting, text="Name:", font=20, bg="#7FB1CE" )#name box
+        self.name_label = Label(self.frame_editting, text="Name:", font=20, bg=colour_2 )#name box
         self.name_label.grid(row=0, column=0)
-        self.name_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.name_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.name_entry.grid(row=1, column=0)
         
-        self.element_label = Label(self.frame_editting, text="Element:", font=20, bg="#7FB1CE")#element box
+        self.element_label = Label(self.frame_editting, text="Element:", font=20, bg=colour_2)#element box
         self.element_label.grid(row=0, column=1)
-        self.element_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.element_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.element_entry.grid(row=1, column=1)
 
-        self.star_label = Label(self.frame_editting, text="Star:", font=20, bg="#7FB1CE")#star box
+        self.star_label = Label(self.frame_editting, text="Star:", font=20, bg=colour_2)#star box
         self.star_label.grid(row=0, column=2)
-        self.star_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.star_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.star_entry.grid(row=1, column=2)
 
-        self.level_label = Label(self.frame_editting, text="Level:", font=20, bg="#7FB1CE")#level box
+        self.level_label = Label(self.frame_editting, text="Level:", font=20, bg=colour_2)#level box
         self.level_label.grid(row=0, column=3)
-        self.level_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.level_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.level_entry.grid(row=1, column=3)
 
-        self.hp_label = Label(self.frame_editting, text="HP:", font=20, bg="#7FB1CE")#HP box
+        self.hp_label = Label(self.frame_editting, text="HP:", font=20, bg=colour_2)#HP box
         self.hp_label.grid(row=0, column=4)
-        self.hp_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.hp_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.hp_entry.grid(row=1, column=4)
 
-        self.atk_label = Label(self.frame_editting, text="Base ATK:", font=20, bg="#7FB1CE")#aTK box
+        self.atk_label = Label(self.frame_editting, text="Base ATK:", font=20, bg=colour_2)#aTK box
         self.atk_label.grid(row=0, column=5)
-        self.atk_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.atk_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.atk_entry.grid(row=1, column=5)
 
-        self.skill_label = Label(self.frame_editting, text="Skill:", font=20, bg="#7FB1CE")#skill box
+        self.skill_label = Label(self.frame_editting, text="Skill:", font=20, bg=colour_2)#skill box
         self.skill_label.grid(row=0, column=6)
-        self.skill_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.skill_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.skill_entry.grid(row=1, column=6)
 
-        self.burst_label = Label(self.frame_editting, text="Burst", font=20, bg="#7FB1CE")#burst box
+        self.burst_label = Label(self.frame_editting, text="Burst", font=20, bg=colour_2)#burst box
         self.burst_label.grid(row=0, column=7)
-        self.burst_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.burst_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.burst_entry.grid(row=1, column=7)
 
-        self.region_label = Label(self.frame_editting, text="Region:", font=20, bg="#7FB1CE")#region box
+        self.region_label = Label(self.frame_editting, text="Region:", font=20, bg=colour_2)#region box
         self.region_label.grid(row=0, column=8)
-        self.region_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.region_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.region_entry.grid(row=1, column=8)
 
-        self.weapon_label = Label(self.frame_editting, text="Weapon", font=20, bg="#7FB1CE") #weapon box
+        self.weapon_label = Label(self.frame_editting, text="Weapon", font=20, bg=colour_2) #weapon box
         self.weapon_label.grid(row=0, column=9)
-        self.weapon_entry = Entry(self.frame_editting, font=20, width=10, bg="#2c77d8")
+        self.weapon_entry = Entry(self.frame_editting, font=20, width=10, bg=colour_3)
         self.weapon_entry.grid(row=1, column=9)
 
         #cutsey frame for character edits button stuff yurrr
@@ -191,7 +208,7 @@ class GenshinSorter:
         self.search_button = ttk.Button(self.page2, text="Search", command=self.search_character, style= 'Custom.TButton')
         self.search_button.pack(side=BOTTOM)#search button 
        
-        self.search_entry = Entry(self.page2, width=35, bg="#2c77d8", font=18)
+        self.search_entry = Entry(self.page2, width=35, bg=colour_3, font=18)
         self.search_entry.pack(side=BOTTOM) #search button entry box yurrr
 
         #INTERACTIVE PAGE 2 TINGS
@@ -542,12 +559,12 @@ class IntroPage(GenshinSorter): #inheritance yurrrrrrrrrrrrrrrr
     def __init__(self, genshin_window):
         super().__init__(genshin_window)
         #title and label fo rpage 1
-        self.page1_title = tk.Label(self.page1, text="HAFSA'S GENSHIN ORGANISER", font=('Arial', 30, "bold"),bg="#7FB1CE")
+        self.page1_title = tk.Label(self.page1, text="HAFSA'S GENSHIN ORGANISER", font=('Arial', 30, "bold"),bg=colour_2)
         self.page1_title.pack(side= TOP, pady=20, expand=True)
-        self.theme_label = tk.Label(self.page1, text="Current Theme: Neuvillette", font=('Arial', 16),bg="#7FB1CE")
+        self.theme_label = tk.Label(self.page1, text="Current Theme: Neuvillette", font=('Arial', 16),bg=colour_2)
         self.theme_label.pack(side= BOTTOM, pady=20, expand=True)
         #Character count #extraStats too good
-        self.character_count_label = tk.Label(self.page1, text= f"TOTAL CHARACTER COUNT: {Character.character_count}" , font=('Arial', 15, "bold"), bg="#7FB1CE") 
+        self.character_count_label = tk.Label(self.page1, text= f"TOTAL CHARACTER COUNT: {Character.character_count}" , font=('Arial', 15, "bold"), bg=colour_2) 
         self.character_count_label.pack(pady=20)
         #cheeky plot for elements on the homepage oui oui
         character_df =Character.make_characters_csv()  #finally get to use the csv i called yurrrrr
@@ -560,7 +577,7 @@ class IntroPage(GenshinSorter): #inheritance yurrrrrrrrrrrrrrrr
         #creates the matplotlib figure and axes
         fig = Figure(figsize=(4,4))  #sets the figure size
         ax = fig.add_subplot(111)  #add a subplot
-        fig.patch.set_facecolor("#7FB1CE")  #set the background of the figure to the lightblue ting
+        fig.patch.set_facecolor(colour_2)  #set the background of the figure to the lightblue ting
         #plots the pie chart
         ax.pie(element_count, labels=element_index, autopct='%0.0f%%', startangle=180)
         element_chart = FigureCanvasTkAgg(fig, frameChart)#embeds the figure into tkinter 
@@ -583,6 +600,3 @@ if __name__ == "__main__":
     intro_page = IntroPage(genshin_window)  #using IntroPage instead of GenshinSorter 
     intro_page.show_page(intro_page.page1) #shows the first page initially
     genshin_window.mainloop() #runs the application
-
-
-
